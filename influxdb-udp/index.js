ad6f166e17edde9fb2025121462bd920083930c1
wangyan <wyvernnot@gmail.com>
2016-01-08 18:36:58 +0800

    use influxdb to record memory via UDP

---
+++ influxdb-udp/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yan on 16-1-8.
+ */
+var dgram = require('dgram');
+var socket = dgram.createSocket('udp4');
+function writeLine(str) {
+    var buffer = new Buffer(str);
+    socket.send(buffer, 0, buffer.length, 4444, 'influxdb', function () {
+        console.log(arguments);
+    })
+}
+
+socket.unref();
+
+setInterval(function () {
+    var mem= process.memoryUsage();
+    writeLine(`memory rss=${mem.rss},heapTotal=${mem.heapTotal},heapUsed=${mem.heapUsed}`)
+}, 5000)
+
